f771731c495d76496976483121d22bb490ef7676
Denis Wells <deniswells59@gmail.com>
2020-09-28 14:35:19 -0700

    menus view

---
+++ src/config/Menu.js
@@ -0,0 +1,10 @@
+import bannerImg from '../../static/menu.jpg';
+
+const CONFIG = {
+  bannerImg,
+  title: 'Menu',
+  text:
+    'At La Mesa, we pride ourselves in the ability not only to deliver food and drinks of high quality but at fair and affordable prices.',
+};
+
+export default CONFIG;
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from 'components/Layout';
+import Banner from 'components/Banner';
+import Flex from 'components/Flex';
+import Button from 'components/Button';
 
-import CONFIG from 'config/Home';
+import { COLORS } from 'theme';
+import menu from '../../static/menus/Brunch.pdf';
+
+import CONFIG from 'config/Menu';
+
+const Wrapper = styled(Flex)`
+  margin: 48px;
+  padding: 24px;
+  border-radius: 2px;
+  background: ${COLORS.PRIMARY};
+`;
+
+const Text = styled.p`
+  color: ${COLORS.WHITE};
+  max-width: 600px;
+  text-align: center;
+  margin: 24px 36px;
+`;
+
+const MenuButton = styled(Button)`
+  padding: 20px 0px;
+  margin: 20px 0px;
+  min-width: 200px;
+`;
 
 export default function Menu() {
   return (
     <Layout>
-      <div>Menu</div>
+      <Banner img={CONFIG.bannerImg}>{CONFIG.title}</Banner>
+
+      <Wrapper column center centerVert>
+        <Text>{CONFIG.text}</Text>
+
+        <a href={menu}>
+          <MenuButton highlight>Menu</MenuButton>
+        </a>
+
+        <a href={menu}>
+          <MenuButton highlight>Bar</MenuButton>
+        </a>
+
+        <a href={menu}>
+          <MenuButton highlight>Dessert</MenuButton>
+        </a>
+      </Wrapper>
     </Layout>
   );
 }
